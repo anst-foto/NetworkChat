76d7ee490e9a0befabbad51814685fc5c58ce2f3
AnSt <starininandrey@gmail.com>
2021-04-12 15:04:21 +0300

    Init Client

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int port = 8005;
+            
+            var ipServer = new IPEndPoint(ip, port);
+            var connection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            connection.Connect(ipServer);
+            
+            ShowInfo("Соединение с сервером успешно установлено");
+
+            var message = "Проверка связи...";
+            var bufferSend = Encoding.Unicode.GetBytes(message);
+            connection.Send(bufferSend);
+            ShowInfo("Сообщение серверу отправлено");
+            
+            message = String.Empty;
+            byte[] buffer = new byte[256];
+            do
+            {
+                var bytes = connection.Receive(buffer);
+                message += Encoding.Unicode.GetString(buffer, 0, bytes);
+            } while (connection.Available > 0);
+                
+            ShowMessage($"Сообщение от сервера - {message}");
+
+        }
+        
+        static void ShowInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
