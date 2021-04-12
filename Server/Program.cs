9d449eaae582a8f8feed805a90fdb645da94956b
AnSt <starininandrey@gmail.com>
2021-04-12 14:51:20 +0300

    Init Server

---
+++ Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Parse("127.0.0.1");
+            const int port = 8005;
+            
+            ShowInfo("Сервер запущен");
+
+            var ipServer = new IPEndPoint(ip, port);
+            var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(ipServer);
+            listenSocket.Listen(10);
+
+            ShowInfo("Сервер ожидает подключение");
+            
+            while (true)
+            {
+                var client = listenSocket.Accept();
+                ShowInfo("Клиент подключился");
+
+                string message = "";
+                byte[] buffer = new byte[256];
+                do
+                {
+                    var bytes = client.Receive(buffer);
+                    message += Encoding.Unicode.GetString(buffer, 0, bytes);
+                } while (client.Available > 0);
+                
+                ShowMessage($"Сообщение от клиента - {message}");
+                
+                message = String.Empty;
+                message = "Сообщение получено";
+                byte[] bufferSend = Encoding.Unicode.GetBytes(message);
+                client.Send(bufferSend);
+                ShowInfo("Сообщение клиенту отправлено");
+                
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                
+                ShowInfo("Соединение с клиентом разорвано");
+            }
+        }
+
+        static void ShowInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
