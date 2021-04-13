e69f658142b51d6ec6db4ced122e9652a0cd33f1
Starinin Andrey <starininandrey@gmail.com>
2021-04-13 15:18:30 +0300

    Auto stash before rebase of "refs/heads/main"

---
+++ TCPLibrary/TCPClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace TCPLibrary
+{
+    public class TCPClient : TCP
+    {
+        public TCPClient() : base() {}
+
+        public TCPClient(string ipAddress, int port) : base(ipAddress, port) {}
+        
+        public TCPClient(Socket socket) : base(socket) {}
+
+        public void Connect()
+        {
+            _socket.Connect(_ipEndPoint);
+        }
+    }
+}
